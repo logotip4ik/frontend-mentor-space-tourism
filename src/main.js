c1ee0f1e277c10b8f014895670d84ff7e1e0541c
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-03-07 16:29:01 +0200

    update: registered global gsap plugin

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import "./assets/styles/global.css";
 import "./assets/styles/transitions.css";
 
 import { ViteSSG } from "vite-ssg";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 import App from "./App.vue";
 import pages from "virtual:generated-pages";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const createApp = ViteSSG(App, { routes: pages });
