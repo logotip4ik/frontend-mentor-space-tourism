cdb17d14ef35195d57af44ed95c7828bad354f73
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-03-08 18:45:17 +0200

    update: added window width hook

---
+++ src/hooks/useWindowWidth.js
@@ -0,0 +1,19 @@
+import { ref, onMounted, onBeforeUpdate, onUpdated, onUnmounted } from "vue";
+
+/**
+ * Reactive hook for getting window width
+ * @param {number} initialWidth this will initial width for render, cuz static renderer does not have window
+ * @returns {[import('vue').Ref<number>]}
+ */
+export default function useWindowWidth(initialWidth = 375) {
+  const width = ref(initialWidth);
+
+  const setWidth = () => (width.value = window.innerWidth);
+
+  onMounted(() => window.addEventListener("resize", setWidth));
+  onUpdated(() => window.addEventListener("resize", setWidth));
+  onBeforeUpdate(() => window.removeEventListener("resize", setWidth));
+  onUnmounted(() => window.removeEventListener("resize", setWidth));
+
+  return [width];
+}
