7dfe1de505ccfe611b671c98ba06fbf446e81c8e
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-03-09 12:40:45 +0200

    update: changed a bit overlay styles

--- src/App.vue
+++ src/App.vue
@@ -56,10 +56,10 @@ function showContent(_, onComplete) {
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
   z-index: 2;
 
+  width: 100%;
+  height: 100%;
   background-color: var(--c-black);
 
   clip-path: inset(0% 50% 0% 0%);
